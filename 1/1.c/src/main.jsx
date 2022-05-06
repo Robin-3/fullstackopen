c2339b9503df28168f25356380131b57bf3d02cb
Robinson Rojas <rrojas@itteamcorp.com>
2022-05-06 11:00:02 -0500

    c) Estado del componente, controladores de eventos

---
+++ 1/1.c/src/main.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
+
+const Hello = ({name, age}) => {
+  const bornYear = () => new Date().getFullYear() - age
+
+  return (
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const Display = ({counter}) => {
+  return (
+    <div>{counter}</div>
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  return (
+    <button onClick={handleClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  const [counter, setCounter] = useState(0)
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+
+      <Display counter={counter}/>
+      <Button
+        handleClick={increaseByOne}
+        text='plus'
+      />
+      <Button
+        handleClick={setToZero}
+        text='zero'
+      />     
+      <Button
+        handleClick={decreaseByOne}
+        text='minus'
+      /> 
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
--- README.md
+++ README.md
@@ -11,10 +11,12 @@
       - 0.6 Nueva nota
 1. [Introduction to React](https://fullstackopen.com/es/part1)
    1. [Introducción a React](https://fullstackopen.com/es/part1/introduccion_a_react)
-      - 1.0 Introducción a React
+      - 1.a Introducción a React
       - 1.1 Información del curso, paso 1
       - 1.2 Información del curso, paso 2
    2. [JavaScript](https://fullstackopen.com/es/part1/java_script)
       - 1.3 Información del curso, paso 3
       - 1.4 Información del curso, paso 4
       - 1.5 Información del curso, paso 5
+   3. [Estado del componente, controladores de eventos](https://fullstackopen.com/es/part1/estado_del_componente_controladores_de_eventos)
+      - 1.c Estado del componente, controladores de eventos
