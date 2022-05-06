dc825c5725e16d38bf3e87a3761c88bc015297a0
Robinson Rojas <rrojas@itteamcorp.com>
2022-05-05 21:43:12 -0500

    1.3: información del curso, paso 3

---
+++ 1/1.3/src/main.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const Header = ({course}) => {
+  return (
+    <h1>{course}</h1>
+  )
+}
+
+const Part = ({part}) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+)
+
+const Content = ({part1, part2, part3}) => {
+  return (
+    <div>
+      <Part part={part1} />
+      <Part part={part2} />
+      <Part part={part3} />
+    </div>
+  )
+}
+
+const Total = ({total}) => (
+  <p>Number of exercises {total}</p>
+)
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content
+        part1={part1}
+        part2={part2}
+        part3={part3}
+      />
+      <Total total={part1.exercises + part2.exercises + part3.exercises} />
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
---
+++ README.md
@@ -0,0 +1,18 @@
+# [Full Stack open 2022](https://fullstackopen.com/es/)
+
+0. [Fundamentals of Web apps](https://fullstackopen.com/es/part0)
+   1. [Información general](https://fullstackopen.com/es/part0/informacion_general)
+   2. [Fundamentos de las aplicaciones web](https://fullstackopen.com/es/part0/fundamentos_de_las_aplicaciones_web)
+      - 0.1 HTML
+      - 0.2 CSS
+      - 0.3 Formularios HTML
+      - 0.4 nueva nota
+      - 0.5 Aplicaciones de una sola página
+      - 0.6 Nueva nota
+1. [Introduction to React](https://fullstackopen.com/es/part1)
+   1. [Introducción a React](https://fullstackopen.com/es/part1/introduccion_a_react)
+      - 1.0 Introducción a React
+      - 1.1 Información del curso, paso 1
+      - 1.2 Información del curso, paso 2
+   2. [JavaScript](https://fullstackopen.com/es/part1/java_script)
+      - 1.3 Información del curso, paso 3
