372501fc601058176db8f3cbaa400a63c2a60f2e
Robinson Rojas <rrojas@itteamcorp.com>
2022-05-06 12:17:12 -0500

    d) Un estado más complejo, depurando aplicaciones React

---
+++ 1/1.d/src/main.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom/client'
+
+const History = ({allClicks}) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        the app is used by pressing the buttons
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      button press history: {allClicks.join(' ')}
+    </div>
+  )
+}
+
+const Display = ({value}) => <div>{value}</div>
+
+const Button = ({ onClick, text }) => {
+  return (
+    <button onClick={onClick}>
+      {text}
+    </button>
+  )
+}
+
+const App = () => {
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
+  const [value, setValue] = useState(10)
+
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
+  }
+
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
+  }
+
+  const setToValue = (newValue) => () => {
+    setValue(newValue)
+  }
+
+  return (
+    <div>
+      {left}
+      <Button onClick={handleLeftClick} text='left' />
+      <Button onClick={handleRightClick} text='right' />
+      {right}
+      <History allClicks={allClicks} />
+      <Display value={value} />
+      <Button onClick={setToValue(1000)} text='thousand' />
+      <Button onClick={setToValue(10)} text='reset' />
+      <Button onClick={setToValue(value + 1)} text='increment' />
+    </div>
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
--- README.md
+++ README.md
@@ -15,8 +15,11 @@
       - 1.1 Información del curso, paso 1
       - 1.2 Información del curso, paso 2
    2. [JavaScript](https://fullstackopen.com/es/part1/java_script)
+      - 1.b JavaScript
       - 1.3 Información del curso, paso 3
       - 1.4 Información del curso, paso 4
       - 1.5 Información del curso, paso 5
    3. [Estado del componente, controladores de eventos](https://fullstackopen.com/es/part1/estado_del_componente_controladores_de_eventos)
       - 1.c Estado del componente, controladores de eventos
+   4. [Un estado más complejo, depurando aplicaciones React](https://fullstackopen.com/es/part1/un_estado_mas_complejo_depurando_aplicaciones_react)
+      - 1.d Un estado más complejo, depurando aplicaciones React
